d2522e47ca50acee0a0b0afe5d521bc0ce547f0e
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2022-10-12 16:01:10 +0100

    Add project files.

---
+++ StackOverflow-Post/BlogPost.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow_Post
+{
+    class BlogPost
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public int Votes { get; private set; }
+
+
+        public void VoteUp()
+        {
+            Votes++;
+        }
+        public void VoteDown()
+        {
+            Votes--;
+        }
+
+        public void PrintFullPost()
+        {
+            Console.WriteLine("Date Posted: " + CreatedTime.ToLongDateString());
+            Console.WriteLine(Title);
+            Console.WriteLine();
+            Console.WriteLine(Description);
+            Console.WriteLine();
+            Console.WriteLine("Votes: " + Votes);
+
+        }
+    }
+}
---
+++ StackOverflow-Post/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StackOverflow_Post;
+
+
+var solutionPost = new BlogPost();
+
+solutionPost.Title = "Adding a README.md file to a c# project in Visual Studio 2015";
+solutionPost.Description = "Look in the top of the solution explorer for the option add hidden files. It should appear with dotted outline.\n If you wish to add it to the project you can right click and press \"add to project\"";
+solutionPost.CreatedTime = new DateTime(2022, 10, 12);
+
+solutionPost.VoteUp();
+solutionPost.VoteUp();
+solutionPost.VoteUp();
+solutionPost.VoteUp();
+solutionPost.VoteUp();
+solutionPost.VoteUp();
+solutionPost.VoteUp();
+solutionPost.VoteUp();
+Console.WriteLine("BlogPost Should Have 8 Votes");
+
+solutionPost.PrintFullPost();
+
+solutionPost.VoteDown();
+solutionPost.VoteDown();
+solutionPost.VoteDown();
+Console.WriteLine("BlogPost Should Have 2 Votes");
+solutionPost.PrintFullPost();
+
